fc3c64462312581892d2a16d4d567c548f1b4b4e
garmanovroman <garmanov-roman@hotmail.com>
2017-04-03 09:41:48 +0400

    Страница помощь

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-
+  !(function(){
+    var $self = $('.dropdown a');
+    $(document).on('click', $self, function(){
+      $self.addClass('active');
+      $self.next().slidetoggle();
+    });
+    console.log('322323');
+    return false;
+  })(jQuery)
 });
