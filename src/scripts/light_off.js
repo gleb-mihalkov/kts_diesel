bd4ad27cc2a8607a757d7c21d132de106b204129
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-04-04 14:59:16 +0300

    Добавление прототипа скрипта, который выключает перекрытие экрана при наведении на активный элемент в шапке.

---
+++ src/scripts/light_off.js
@@ -0,0 +1,16 @@
+/// -----------------------------------------
+/// Добавляет обработку атрибута "light-off".
+/// -----------------------------------------
+!(function($) {
+
+  $(document)
+
+    .on('mouseenter', '[data-light-off]', function() {
+      
+    })
+
+    .on('mouseout', '[data-light-off]', function() {
+      
+    });
+
+})(window.jQuery);
