a6db68358a7da69c9d87a27f2254f37d338f6303
garmanovroman <garmanov-roman@hotmail.com>
2017-04-07 15:03:52 +0300

    Блоки детальной карточки

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,25 +1,15 @@
-$(document).ready(function(){
-  !(function(){
-    $(document).on('click','.dropdown > a' , function(e){
-      var $self = $(e.target);
-      $self.toggleClass('active');
-      $self.next('ul').slideToggle();
-      return false;
-    });
-  })(jQuery)
-  $(function(){
-    var $tab_nav = $('.tabs_nav .tabs_nav__item');
-    var $tab_content = $('.tabs_content .tabs_content__item');
-    $(document).on('click', $tab_nav, function(e){
+!(function($) {
+  $('.tabs_nav .tabs_nav__item:first').addClass('active');
+  $('.tabs_content .tabs_content__item:first').addClass('active');
+    $(document).on('click', '.tabs_nav .tabs_nav__item', function(e){
+      var $tab_nav = $('.tabs_nav .tabs_nav__item');
+      var $tab_content = $('.tabs_content .tabs_content__item');
       var $self = $(e.target);
       $tab_nav.removeClass('active');
       $self.addClass('active');
       var $index = $(e.target).index();
       $tab_content.removeClass('active');
       $tab_content.eq($index).addClass('active');
+      return false;
     });
-    $('.tabs_nav .tabs_nav__item:first').addClass('active');
-    $('.tabs_content .tabs_content__item:first').addClass('active');
-    return false;
-  })
-});
+})(window.jQuery);
