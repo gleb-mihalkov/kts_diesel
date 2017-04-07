11d750d565471c0db14bc465f922a27b9b589966
garmanovroman <garmanov-roman@hotmail.com>
2017-04-07 17:43:39 +0300

    Исправление контактов

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,4 +1,13 @@
-!(function($) {
+$(document).ready(function(){
+  !(function(){
+    $(document).on('click','.dropdown > a' , function(e){
+      var $self = $(e.target);
+      $self.toggleClass('active');
+      $self.next('ul').slideToggle();
+      return false;
+    });
+  })(jQuery)
+  !(function($) {
   $('.tabs_nav .tabs_nav__item:first').addClass('active');
   $('.tabs_content .tabs_content__item:first').addClass('active');
     $(document).on('click', '.tabs_nav .tabs_nav__item', function(e){
@@ -12,9 +21,10 @@
       $tab_content.eq($index).addClass('active');
       return false;
     });
-})(window.jQuery);
-!(function($) {
-  $(document).on('click', 'a', function(){
-    return false;
-  })
-})(window.jQuery);
+  })(window.jQuery);
+  !(function($) {
+    $(document).on('click', 'a', function(){
+      return false;
+    })
+  })(window.jQuery);
+});
